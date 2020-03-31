33575e8dbc62705ed3e102fb549e6ae173f1ce06
dkihe <dnkkihe@gmail.com>
2020-03-30 15:51:51 -1000

    Finished draft of win screen

--- Tank Game/Assets/Scripts/GameController.cs
+++ Tank Game/Assets/Scripts/GameController.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
     // Start is called before the first frame update
     private GameObject[] players;
+    public Text uiObject;
     void Start() {
-        players = GameObject.FindGameObjectsWithTag ("Player");
-        Debug.Log(players.Length);
     }
 
     // Update is called once per frame
     void Update() {
-        
+        checkNumPlayers();
+    }
+
+    void checkNumPlayers() {
+        players = GameObject.FindGameObjectsWithTag ("Player");
+        if (players.Length <= 1) {
+            string winner = players[0].ToString();
+            string player = winner.Split(' ')[0];
+            string number = winner.Split(' ')[1];
+
+            // uiObject.text = "AIBDIBDKABD";
+            uiObject.text = player + " " + number + " WINS";
+            // uiObject.text = " WINS";
+        }
     }
 }
